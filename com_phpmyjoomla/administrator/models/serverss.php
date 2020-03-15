5e9354567d90cf53b70c247ac851520429be5417
luisorozoli <luisorozoli@gmail.com>
2020-03-15 18:54:57 +0100

    v3.0.0 release

--- com_phpmyjoomla/administrator/models/serverss.php
+++ com_phpmyjoomla/administrator/models/serverss.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2.1.0
+ * @version     3.0.0
  * @package     com_phpmyjoomla
  * @copyright   Copyright (c) 2014-2020. Luis Orozco Olivares / phpMyjoomla. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
